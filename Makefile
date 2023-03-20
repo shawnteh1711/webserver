3ce11a9f9b72d458a9b2f12bf6556a5dd4d81414
Lim Eng chuan <leng-chu@u91z01s06.42kl.edu.my>
2023-03-20 12:37:40 +0800

    setup branch for alec

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: steh <steh@student.42.fr>                  +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/03/10 21:12:27 by steh              #+#    #+#              #
-#    Updated: 2023/03/12 22:11:52 by steh             ###   ########.fr        #
+#    Updated: 2023/03/20 12:35:43 by leng-chu         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -14,27 +14,14 @@ NAME				=	webserv
 SRCS_DIR			=	srcs
 SRCS				=	$(wildcard $(SRCS_DIR)/*.cpp) $(wildcard $(SRCS_DIR)/*/*.cpp)
 
-# SRCS				=	$(wildcard $(SRCS_DIR)/*.cpp) \
-# 						$(wildcard $(SRCS_DIR)/server/*.cpp) \
-# 						$(wildcard $(SRCS_DIR)/config/*.cpp) \
-
 OBJS_DIR			=	objs
 OBJS				=	$(patsubst $(SRCS_DIR)/%.cpp, $(OBJS_DIR)/%.o, $(SRCS))
 DEPENDS_DIR			=	dependencies
 DEPENDS				=	$(patsubst $(SRCS_DIR)/%.cpp, $(OBJS_DIR)/%.d, $(SRCS))
-CXXFLAGS			=	-Wall -Wextra -Werror -std=c++98 -pedantic -fsanitize=address
-CXX					=	c++
+CXXFLAGS			+=	-Wall -Wextra -Werror -std=c++98 -pedantic -fsanitize=address
 INCLUDES			=	-I$(SRCS_DIR) $(patsubst %, -I%, $(wildcard $(SRCS_DIR)/*/))
 
-# INCLUDES			=	-I$(SRCS_DIR) \
-# 						-I$(SRCS_DIR)/server \
-# 						-I$(SRCS_DIR)/logger \
-# 						-I$(SRCS_DIR)/request \
-# 						-I$(SRCS_DIR)/response \
-# 						-I$(SRCS_DIR)/config \
-
 all					:	$(NAME)
-						./$(NAME) ./config/nginx.conf
 
 $(NAME)				:	$(OBJS)
 						$(CXX) $(CXXFLAGS) $^ -o $@
--- srcs/main.cpp
+++ srcs/main.cpp
@@ -3,47 +3,20 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: steh <steh@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: leng-chu <-chu@student.42kl.edu.my>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/03/10 20:55:27 by steh              #+#    #+#             */
-/*   Updated: 2023/03/13 11:54:18 by steh             ###   ########.fr       */
+/*   Created: 2023/03/20 12:36:05 by leng-chu          #+#    #+#             */
+/*   Updated: 2023/03/20 12:37:07 by leng-chu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "webserv.hpp"
+#include <iostream>
 
-int main(int argc, char* argv[])
+using std::cout;
+using std::cin;
+using std::endl;
+
+int	main(void)
 {
-    string  config;
-
-    if (argc < 2 || argc > 2)
-    {
-        cerr << "./webserv [configuration file]" << endl;
-        return (EXIT_FAILURE);
-    }
-    config = argv[1];
-    ifstream config_file(config);
-    if (!config_file.is_open())
-    {
-        cerr << "Error: Failed to open configuration file" << endl;
-        return (EXIT_FAILURE);
-    }
-    else
-    {
-        cout << "reading...." << endl;
-        server server(config_file);
-        server.setup();
-    }
-    
-    // Create a new socket for incoming connections
-    // m_socket listen_socket;
-
-    // Parse configuration file
-    // TODO: implement configuration file parsing
-
-    // Start webserver
-    // TODO: implement webserver
-
-    // system("leaks webserv");
-    return (EXIT_SUCCESS);
+	cout << "hello" << endl;
 }
