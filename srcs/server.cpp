7f44ab1e8c03d5a7c3685a7df13fc03b8018d571
Shawn Teh <steh@u91z03s02.42kl.edu.my>
2023-04-08 18:25:57 +0800

    update

--- srcs/server.cpp
+++ srcs/server.cpp
@@ -6,7 +6,7 @@
 /*   By: steh <steh@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/17 17:51:13 by leng-chu          #+#    #+#             */
-/*   Updated: 2023/04/08 17:40:46 by steh             ###   ########.fr       */
+/*   Updated: 2023/04/08 18:24:50 by steh             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -569,7 +569,8 @@ void	Server::sendClient(int & client_fd, string & method_type,
 	cout << YELLOW << "Full path: " << full_path << RESET << endl;
 	if (!isMethod(method_type))
 		sendCustomErrorResponse(client_fd, 400, s, root_path);
-	else if (root_path == "" || !isLocationExist(s, uri_path))
+	// else if (root_path == "" || !isLocationExist(s, uri_path))
+	else if (root_path == "")
 		sendCustomErrorResponse(client_fd, 404, s, root_path);
 	else if (isCgiRequest(uri_path, s, cgi_path))
 	{
@@ -604,16 +605,16 @@ void	Server::sendClient(int & client_fd, string & method_type,
 		// root need to be tmp/www again
 		// ur parsing has any trimmming function till the last /?
 		// is "\\" same as "/" same result , why use \\?
-		cout << "uri_path before: " << uri_path << endl;
-		int pos = uri_path.find_last_not_of("/");
-		cout << "char: " << uri_path[pos] << endl;
-		cout << "pos: " << pos << endl;
-		string newfile = uri_path.substr(pos, uri_path.length());
-		string folderpath = uri_path.substr(0, pos - 1);
-		if (((root_path = getLocationRoot(folderpath, s)) == ""))
-			root_path = servers[s].root;
-		full_path = root_path + newfile;
-		cout << "POST PATH:" << full_path << endl;
+		// cout << "uri_path before: " << uri_path << endl;
+		// int pos = uri_path.find_last_not_of("/");
+		// cout << "char: " << uri_path[pos] << endl;
+		// cout << "pos: " << pos << endl;
+		// string newfile = uri_path.substr(pos, uri_path.length());
+		// string folderpath = uri_path.substr(0, pos - 1);
+		// if (((root_path = getLocationRoot(folderpath, s)) == ""))
+		// 	root_path = servers[s].root;
+		// full_path = root_path + newfile;
+		// cout << "POST PATH:" << full_path << endl;
 		sendCustomResponse(client_fd, full_path);
 	}
 	else if (method_type == "DELETE")
@@ -623,6 +624,17 @@ void	Server::sendClient(int & client_fd, string & method_type,
 			sendCustomErrorResponse(client_fd, 500, s, root_path);
 		else
 		{
+			cout << "uri_path before: " << uri_path << endl;
+			int pos = uri_path.find_last_not_of("/");
+			cout << "char: " << uri_path[pos] << endl;
+			cout << "pos: " << pos << endl;
+			string newfile = uri_path.substr(pos, uri_path.length());
+			string folderpath = uri_path.substr(0, pos - 1);
+			if (((root_path = getLocationRoot(folderpath, s)) == ""))
+				root_path = servers[s].root;
+			full_path = root_path + newfile;
+			deleteFile(full_path.c_str());
+			cout << "POST PATH:" << full_path << endl;
 			cout << GREEN"METHOD_TYPE: "YELLOW << method_type << RESET << endl; 
 			sendCustomResponse(client_fd, full_path);
 		}
@@ -756,6 +768,7 @@ int		Server::isLocationExist(int const & svr_id, const string & s_uri)
 	vector<string>::iterator it, ite;
 	string newslash = addslash(s_uri);
 
+	cout << "svr_id: " << svr_id << " s_uri: " << s_uri << " newslash: " << newslash << endl;
 	if (s_uri == "")
 		return (1);
 	it = servers[svr_id].urlLocation.begin();
