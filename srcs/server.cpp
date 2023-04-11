7c9fa27a2ca92cad1bf3dcfb38b4b10a65087dab
Shawn Teh <steh@u91z03s02.42kl.edu.my>
2023-04-11 12:22:18 +0800

    update

--- srcs/server.cpp
+++ srcs/server.cpp
@@ -6,7 +6,7 @@
 /*   By: steh <steh@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/17 17:51:13 by leng-chu          #+#    #+#             */
-/*   Updated: 2023/04/11 11:57:20 by steh             ###   ########.fr       */
+/*   Updated: 2023/04/11 12:22:01 by steh             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -173,6 +173,7 @@ void generate_listing(t_server *s_t, string &listing)
 	DIR				*dir;
 	struct dirent	*ent;
    
+	cout << "s_t->full_path: " << s_t->full_path << endl;
 	if ((dir = opendir(s_t->full_path.c_str())) != NULL)
 	{
 	    listing += "<p>Path: " + s_t->full_path + "</p>";
@@ -182,11 +183,14 @@ void generate_listing(t_server *s_t, string &listing)
 	 	   if (name == "." || name == "..")
 	 		   continue;
 	 	   string link = s_t->client_uri;
-	 	   if (s_t->client_uri[s_t->client_uri.length() - 1] != '/')
+	 	   cout << YELLOW << "before link: " << link << RESET << endl;
+		   cout << "s_t->client_uri[s_t->client_uri.length() - 1]: " << s_t->client_uri[s_t->client_uri.length() - 1] << endl;
+	 	   if (s_t->client_uri[s_t->client_uri.length() - 1] != '/') //what is this for? just to add / if last one is not /
 	 		   link += "/";
 	 	   link += name;
-	 	   cout << "link: " << link << endl;
+	 	   cout << YELLOW << "link: " << link << RESET << endl;
 	 	   listing += "<li><a href=\"" + link + "\">" + name + "</a></li>";
+		   cout << RED << "listing: " << listing << RESET << endl;
 	 	  // listing += "<li>" + string(ent->d_name) + "</li>"; //pr
 	    }
 	    closedir(dir);
